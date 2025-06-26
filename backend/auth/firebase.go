3042ca458737da9aea353c8d2a2ee2d71150d98e
Malhar <malhargharat11@gmail.com>
2025-06-27 02:28:39 +0530

    refactor: allow firebase auth from env var for native deployment

--- backend/auth/firebase.go
+++ backend/auth/firebase.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	"log"
+	"os"
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
@@ -12,8 +13,21 @@ import (
 var AuthClient *auth.Client
 
 func InitFirebaseAuth() {
-	// IMPORTANT: Make sure 'serviceAccountKey.json' is in your .gitignore
-	opt := option.WithCredentialsFile("serviceAccountKey.json")
+	var opt option.ClientOption
+
+	// Render provides the content of a "Secret File" via an environment variable.
+	// We check if that variable exists.
+	firebaseCredentials := os.Getenv("FIREBASE_CREDENTIALS")
+
+	if firebaseCredentials != "" {
+		// If it exists (we're on Render), initialize from the JSON content.
+		log.Println("Initializing Firebase Auth from FIREBASE_CREDENTIALS environment variable...")
+		opt = option.WithCredentialsJSON([]byte(firebaseCredentials))
+	} else {
+		// Otherwise (we're running locally), initialize from the file.
+		log.Println("Initializing Firebase Auth from serviceAccountKey.json file...")
+		opt = option.WithCredentialsFile("serviceAccountKey.json")
+	}
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
